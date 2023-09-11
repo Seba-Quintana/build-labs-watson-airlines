bb7ea2c2de50e548765dabfe87b78264673a799e
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2023-09-11 16:17:23 -0300

    add flights routes

--- source/back-end/src/routes/flights.routes.js
+++ source/back-end/src/routes/flights.routes.js
@@ -4,19 +4,23 @@ import controller from "../controllers/flights.controller.js";
 const router = express.Router();
 
 router.get("/originAirport", (req, res) => {
-	controller.SearchByOriginAirport(req, res);
+	controller.search_by_origin_airport(req, res);
 });
 
 router.get("/destAirport", (req, res) => {
-	controller.SearchByDestinationAirport(req, res);
+	controller.search_by_destination_airport(req, res);
+});
+
+router.get("/oriDestAirport", (req, res) => {
+	controller.search_by_ori_and_dest_airport(req, res);
 });
 
 router.get("/depDate", (req, res) => {
-	controller.SearchByDepartureDate(req, res);
+	controller.search_by_departure_date(req, res);
 });
 
-router.get("/Info", (req, res) => {
-	controller.Info(req, res);
+router.get("/info", (req, res) => {
+	controller.flight_info(req, res);
 });
 
 export default router;
