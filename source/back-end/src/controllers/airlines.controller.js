330a0681befcf574dd60a07d589b3d1b712ab35e
Seba-Quintana <SEBASTIAN.QUINTANA@correo.ucu.edu.uy>
2023-09-11 16:26:31 -0300

    update airlines controller

--- source/back-end/src/controllers/airlines.controller.js
+++ source/back-end/src/controllers/airlines.controller.js
@@ -4,35 +4,22 @@ import { request, response } from "express";
 import Airline from "../models/airline.schema.js";
 
 /**
- * Airlines Controller
- * @param {JSON} req request associated airlines
+ * searches for associated airlines with Watson Airlines
  * @param {JSON} res response list of associated airlines
- * @returns {JSON} return description
+ * @returns {JSON} return airlines data
  */
-const AssociatedAirlines = async (req = request, res = response) => {
-    // Returns list of Sample objects under "result" field
-    /* #swagger.responses[200] = {
-            "description": "OK",
-            "content": {
-              "application/json": {
-                "schema": {
-                    "type" : "object",
-                    "properties" : {
-                        "result" : {
-                            "type": "array",
-                            "items": {
-                              "$ref": "#/components/schemas/Sample"
-                            }
-                        }
-                    }
-                }
-              }
-            }
-        }
-    */
+const associated_airlines = async (req = request, res = response) => {
     try {
-		// find() returns every airline
-        let result = await Airline.find({});
+		// find({}) returns every airline
+        const result = await Airline.find({});
+
+		if (result.length === 0) {
+            return res.status(404).json({
+                status: 'Not Found',
+                message: 'No airlines found.'
+            });
+        }
+
         res.json ({
             result : result
         });
@@ -44,34 +31,13 @@ const AssociatedAirlines = async (req = request, res = response) => {
 };
 
 /**
- * Airlines Controller
- * @param {JSON} req request information about Watson Airlines
+ * gives short description about Watson Airlines
  * @param {JSON} res response Watson Airlines description
  * @returns {JSON} return description
  */
-const About = async (req = request, res = response) => {
-    // Returns list of Sample objects under "result" field
-    /* #swagger.responses[200] = {
-            "description": "OK",
-            "content": {
-              "application/json": {
-                "schema": {
-                    "type" : "object",
-                    "properties" : {
-                        "result" : {
-                            "type": "array",
-                            "items": {
-                              "$ref": "#/components/schemas/Sample"
-                            }
-                        }
-                    }
-                }
-              }
-            }
-        }
-    */
+const about = async (req = request, res = response) => {
     try {
-        let result = "Watson Airlines is a one of the largest airlines in America. With over 30 years of history, we connect people to opportunities while expanding the understanding of our planet and the people within it. We offer our one-of-a-kind value and Hospitality at over 50 airports across more than 15 countries. In addition, we are members of the International Air Transport Association (IATA), a trade association that represents over 300 airlines, equivalent to about 83% of total air traffic. This allows us to operate safely, securely, efficiently, and economically under clearly defined rules. \nWe are pioneers in the usage of technology, and actively advocate for its usage to improve our customer's experience.";
+        const result = "Watson Airlines is a one of the largest airlines in America. With over 30 years of history, we connect people to opportunities while expanding the understanding of our planet and the people within it. We offer our one-of-a-kind value and Hospitality at over 50 airports across more than 15 countries. In addition, we are members of the International Air Transport Association (IATA), a trade association that represents over 300 airlines, equivalent to about 83% of total air traffic. This allows us to operate safely, securely, efficiently, and economically under clearly defined rules. \nWe are pioneers in the usage of technology, and actively advocate for its usage to improve our customer's experience.";
         res.json ({
             result : result
         });
@@ -82,4 +48,4 @@ const About = async (req = request, res = response) => {
     }
 };
 
-export default { AssociatedAirlines, About };
+export default { associated_airlines, about };
